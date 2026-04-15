534cc02a15bb1bc66b433b20efce354dc359ad56
AyaSheta13 <ayasheta965@gmail.com>
2026-04-16 01:00:39 +0200

    Edit UI

---
+++ Assets/Scripts/UI Scripts/CustomNav.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomNav : MonoBehaviour
+{
+    public GameObject defaultButton; // الزر المحدد تلقائياً عند البدء
+
+    void Start()
+    {
+        if (defaultButton != null)
+            EventSystem.current.SetSelectedGameObject(defaultButton);
+    }
+
+    void Update()
+    {
+        // الاختيار بزر Enter فقط
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            GameObject current = EventSystem.current.currentSelectedGameObject;
+            if (current != null)
+            {
+                Button btn = current.GetComponent<Button>();
+                if (btn != null && btn.interactable)
+                    btn.onClick.Invoke();
+            }
+        }
+    }
+}
